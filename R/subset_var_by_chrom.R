ad215579ab15a0587147aa434348d3cff66f3383
Jai Broome <jb@jaibroome.com>
2022-04-29 16:50:26 -0700

    add ability to pass multiple chromosomes

--- R/subset_var_by_chrom.R
+++ R/subset_var_by_chrom.R
@@ -12,7 +12,7 @@ subset_var_by_chrom <- function(variant_id, chrom, gds) {
     id = SeqArray::seqGetData(gds, "variant.id"),
     chromosome = SeqArray::seqGetData(gds, "chromosome")
   )
-  chrom_map <- dplyr::filter(chrom_map, chromosome == chrom)
+  chrom_map <- dplyr::filter(chrom_map, chromosome %in% chrom)
   var_id_out <- variant_id[variant_id %in% chrom_map$id]
   return(var_id_out)
 }
