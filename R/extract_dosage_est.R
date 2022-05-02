b403b18d9d8ce71197ced1d05d12febc07d60b71
Jai Broome <jb@jaibroome.com>
2022-05-02 13:00:36 -0700

    Add input length check and test

--- R/extract_dosage_est.R
+++ R/extract_dosage_est.R
@@ -8,8 +8,12 @@
 #' @export
 
 extract_dosage_est <- function(gds, chrom, from.bp, to.bp) {
+  if (length(chrom) != length(from.bp) | length(chrom) != length(to.bp)) {
+    stop(LENGTH_MISMATCH_ESTRACT_DOSAGE_EST)
+  }
   SeqArray::seqSetFilterChrom(gds, chrom, from.bp = from.bp, to.bp = to.bp)
   out <- as.data.frame(SeqArray::seqGetData(gds, "annotation/format/DS"))
+  colnames(out) <- make.names(SeqArray::seqGetData(gds, "annotation/id"))
   out$sample.id <- SeqArray::seqGetData(gds, "sample.id")
   seqResetFilter(gds)
   return(out)
